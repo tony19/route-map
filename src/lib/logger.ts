9aa16a3f01ca3e812181297b10be7157ba9e1739
Tony Trinh <tony19@gmail.com>
2018-05-12 03:30:14 -0500

    Colorize console output

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,2 +1,17 @@
 import winston from 'winston';
-export default winston;
+
+const config: any = (winston as any)['config']; // tslint:disable-line:no-string-literal
+const logger = new ((winston as any)['Logger']) ({ // tslint:disable-line:no-string-literal
+  transports: [
+    new (winston.transports.Console) ({
+      formatter(options) {
+        // - Return string will be passed to logger.
+        // - Optionally, use options.colorize(options.level, <string>) to
+        //   colorize output based on the log level.
+        return config.colorize(options.level, `${options.level}: ${options.message}`);
+      },
+    }),
+  ],
+});
+
+export default logger;
