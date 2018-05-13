6852695e8c88d520614d2563b291bcd99a3b1be6
Tony Trinh <tony19@gmail.com>
2018-05-13 02:27:44 -0500

    Use module-alias to resolve paths

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// register module path mappings (in package.json)
+require("module-alias/register");
 var setup_routes_1 = require("@/lib/setup-routes");
 exports.setupRoutes = setup_routes_1.setupRoutes;
 var json_1 = require("@/lib/handlers/json");
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// register module path mappings (in package.json)
+import 'module-alias/register';
+
 export { setupRoutes } from '@/lib/setup-routes';
 export { IRouteConfig } from '@/lib/route-config';
 export { json } from '@/lib/handlers/json';
