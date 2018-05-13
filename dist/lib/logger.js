45da19554c94b897b98ebc0da2a407bb89426f6d
Tony Trinh <tony19@gmail.com>
2018-05-13 02:29:32 -0500

    Update dist

--- dist/lib/logger.js
+++ dist/lib/logger.js
@@ -3,19 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var winston_1 = __importDefault(require("winston"));
-var config = winston_1.default['config']; // tslint:disable-line:no-string-literal
-var logger = new (winston_1.default['Logger'])({
-    transports: [
-        new (winston_1.default.transports.Console)({
-            formatter: function (options) {
-                // - Return string will be passed to logger.
-                // - Optionally, use options.colorize(options.level, <string>) to
-                //   colorize output based on the log level.
-                return config.colorize(options.level, options.level + ": " + options.message);
-            },
-        }),
-    ],
-});
+var chalk_1 = __importDefault(require("chalk"));
+var Logger = /** @class */ (function () {
+    function Logger() {
+    }
+    Logger.prototype.log = function (msg) {
+        console.log(msg);
+    };
+    Logger.prototype.debug = function (msg) {
+        this.log(chalk_1.default.gray(msg));
+    };
+    Logger.prototype.info = function (msg) {
+        this.log(chalk_1.default.white(msg));
+    };
+    Logger.prototype.warn = function (msg) {
+        this.log(chalk_1.default.yellow(msg));
+    };
+    Logger.prototype.error = function (msg) {
+        this.log(chalk_1.default.red(msg));
+    };
+    return Logger;
+}());
+var logger = new Logger();
 exports.default = logger;
 //# sourceMappingURL=logger.js.map
