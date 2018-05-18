99a0d16df9416a2a9e35ed659cf8477cb73285cb
Tony Trinh <tony19@gmail.com>
2018-05-17 21:12:48 -0500

    Add jest-extended helper

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
+  setupTestFrameworkScriptFile: 'jest-extended',
   snapshotSerializers: [
     'jest-serializer-vue'
   ],
